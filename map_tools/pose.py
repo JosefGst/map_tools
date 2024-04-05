2728737ed03c255885d8c290be740dfa66fce37c
josefgst <josefgstoettner@live.at>
2024-04-05 17:05:06 +0800

    working

--- map_tools/pose.py
+++ map_tools/pose.py
@@ -3,9 +3,12 @@
 import argparse
 import rospy
 from geometry_msgs.msg import PoseWithCovarianceStamped
+import tf
 import time
 import sys
 import math
+
+import tf.transformations
 
 
 class SavePoses(object):
@@ -18,14 +21,24 @@ class SavePoses(object):
     def pose_callback(self, msg):
         self._pose = msg
 
-    def write_to_file(self, pose_name, output_file):
+    def write_to_file(self, pose_name, output_file, euler=False):
         time.sleep(1)
         with open(output_file, "a") as file:
-            file.write(str(pose_name) + " = [" + format(self._pose.pose.pose.position.x, ".2f"))
+            file.write(
+                str(pose_name) + " = [" + format(self._pose.pose.pose.position.x, ".2f")
+            )
             file.write(", " + format(self._pose.pose.pose.position.y, ".2f"))
+            if euler:  # write the euler angles
+                _, _, yaw = tf.transformations.euler_from_quaternion([0, 0,
+                    self._pose.pose.pose.orientation.z,
+                    self._pose.pose.pose.orientation.w]
+                )
+                file.write(", " + format(math.degrees(yaw), ".2f"))
 
-            file.write(", " + format(self._pose.pose.pose.orientation.z, ".2f"))
-            file.write(", " + format(self._pose.pose.pose.orientation.w, ".2f"))
+            else:  # write the quaternion
+                file.write(", " + format(self._pose.pose.pose.orientation.z, ".2f"))
+                file.write(", " + format(self._pose.pose.pose.orientation.w, ".2f"))
+
             file.write("]\n")
 
         rospy.loginfo("Written Pose to poses.txt")
@@ -34,7 +47,7 @@ class SavePoses(object):
 def init_cli(args):
     parser = argparse.ArgumentParser(
         prog="pose",
-        description="Record the current pose of the robot and save it to a file.",
+        description="Record the current amcl_pose and orientation in quaternion of the robot and save it to a file. \nExample usage: rosrun map_tools pose.py \nExample usage: rosrun map_tools pose.py -o my_pose.txt -e ", formatter_class=argparse.RawTextHelpFormatter,
     )
     parser.add_argument(
         "name",
@@ -49,6 +62,12 @@ def init_cli(args):
         default="poses.txt",
         help='Outputs to the specified file path eg."my_pose.txt".',
     )
+    parser.add_argument(
+        "-e",
+        "--euler",
+        action="store_true",
+        help="Output the orientation in Euler angles.",
+    )
     parser.add_argument("-v", "--version", action="version", version="%(prog)s 0.1.0")
 
     return parser.parse_args(args)
@@ -58,7 +77,7 @@ def main():
     rospy.init_node("pose_recorder", log_level=rospy.INFO)
     args = init_cli(sys.argv[1:])
     save_pose_object = SavePoses()
-    save_pose_object.write_to_file(args.name, args.output)
+    save_pose_object.write_to_file(args.name, args.output, args.euler)
 
 
 if __name__ == "__main__":
