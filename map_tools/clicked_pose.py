21fbdc554eac197bd74ee85684546d97338f5550
josefgst <josefgstoettner@live.at>
2024-04-05 17:40:56 +0800

    Add command line interface to clicked_pose.py

--- map_tools/clicked_pose.py
+++ map_tools/clicked_pose.py
@@ -1,11 +1,14 @@
 #! /usr/bin/env python3
 
+from clicked_pose_cli import init_cli
 import rospy
 from pose import SavePoses
 from geometry_msgs.msg import PoseWithCovarianceStamped
+import sys
 
 class SaveClickedPoses(SavePoses):
     def __init__(self):
+        self.args = init_cli(sys.argv[1:])
         self.pose_counter = 0
         self._pose_sub = rospy.Subscriber(
             "clicked_pose", PoseWithCovarianceStamped, self.pose_callback
@@ -13,9 +16,8 @@ class SaveClickedPoses(SavePoses):
 
     def pose_callback(self, msg):
         self._pose = msg
-        self.write_to_file("pose" + str(self.pose_counter), "poses.txt")
+        self.write_to_file("pose" + str(self.pose_counter), self.args.output, self.args.euler)
         self.pose_counter += 1
-
 
 
 def main():
---
+++ map_tools/clicked_pose_cli.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python3
+
+import argparse
+
+
+def init_cli(args):
+    parser = argparse.ArgumentParser(
+        prog="pose",
+        description="Subscribe to the clicked_pose topic and save it to a file. \nExample usage: rosrun map_tools clicked_pose.py \nExample usage: rosrun map_tools clicked_pose.py -o my_pose.txt -e ", formatter_class=argparse.RawTextHelpFormatter,
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        default="poses.txt",
+        help='Outputs to the specified file path eg."my_pose.txt".',
+    )
+    parser.add_argument(
+        "-e",
+        "--euler",
+        action="store_true",
+        help="Output the orientation in Euler angles.",
+    )
+    parser.add_argument("-v", "--version", action="version", version="%(prog)s 0.1.0")
+
+    return parser.parse_args(args)
